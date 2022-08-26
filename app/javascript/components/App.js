06cf4e7c03cbe8a8cbdf1274cbaba4d45d694bf7
Shinji Okazaki <oshinji871@gmail.com>
2022-08-26 18:51:40 +0900

    Modified git3

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,3 +1,38 @@
 import React from 'react';
-const HelloMessage = ({ name }) => <h1>Hello, {name}!</h1>;
-export default HelloMessage;
+import { Switch, Route, Link } from 'react-router-dom';
+import { AddTodo } from './AddTodo'
+import { TodoList } from './TodoList'
+import { EditTodo } from './EditTodo'
+import './App.css'
+
+
+const App = () => {
+    return (
+        <nav>
+            <div>
+                Todo
+            </div>
+            <ul>
+                <li>
+                    <Link to="/todos">
+                        Todos
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/todos/new">
+                        New Todo
+                    </Link>
+                </li>
+            </ul>
+            <div>
+                <Switch>
+                    <Route exact path="/todos" components={TodoList} />
+                    <Route exact path="/todos/new" components={AddTodo} />
+                    <Route path="/todos/:id/edit" components={EditTodo} />
+                </Switch>
+            </div>
+        </nav>
+    )
+}
+    
+export default App;
