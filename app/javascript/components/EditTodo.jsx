b2b2367b8446610515b31ba5a64f6d563121bade
Shinji Okazaki <oshinji871@gmail.com>
2022-08-29 22:28:02 +0900

    Created EditTodo

--- app/javascript/components/EditTodo.jsx
+++ app/javascript/components/EditTodo.jsx
@@ -1,8 +1,176 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import styled from 'styled-components';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router';
+import { useParams } from 'react-router';
+
+const InputName = styled.input`
+  font-size: 20px;
+  width: 100%;
+  height: 40px;
+  padding: 2px 7px;
+  margin: 12px 0;
+`
+
+const CurrentStatus = styled.div`
+  font-size: 19px;
+  margin: 8px 0 12px 0;
+  font-weight: bold;
+`
+
+const IsCompletedButton = styled.button`
+  color: #fff;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  background-color: #f2a115;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
+
+const EditButton = styled.button`
+  color: #fff;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  margin: 0 10px;
+  background-color: #0ac620;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
+
+const DeleteButton = styled.button`
+  color: #fff;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  background-color: #f54242;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
 
 function EditTodo() {
+  const history = useNavigate();
+  const a = useParams();
+  // console.log(a);
+  const params = Number(a.id)
+  // console.log(params);
+  
+
+  const initialTodoState = {
+    id: null,
+    name: "",
+    is_completed: false
+  }
+
+  const [currentTodo, setCurrentTodo] = useState(initialTodoState);
+
+  const notify = () => {
+    toast.success('Todo successfully updated!', {
+      position: 'bottom-center',
+      hideProgressBar: true,
+    })
+  }
+
+  const getTodo = id => {
+    axios.get(`/api/v1/todos/${id}`)
+    .then(resp => {
+      setCurrentTodo(resp.data);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+  }
+
+  useEffect(() => {
+    getTodo(params);
+  }, []);
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setCurrentTodo({ ...currentTodo, [name]: value });
+  }
+
+  const updateIsCompleted = val => {
+    var data = {
+      id: val.id,
+      name: val.name,
+      is_completed: !val.is_completed
+    }
+
+    axios.patch(`/api/v1/todos/${val.id}`, data)
+    .then(resp => {
+      setCurrentTodo(resp.data);
+    })
+  }
+
+  const updateTodo = () => {
+    axios.patch(`/api/v1/todos/${currentTodo.id}`, currentTodo)
+    .then(resp => {
+      notify();
+      history('/todos');
+    })
+    .catch(e => {
+      console.log(e);
+    })
+  }
+
+  const deleteTodo = () => {
+    const sure = window.confirm('Are you sure?');
+    if(sure) {
+      axios.delete(`/api/v1/todos/${currentTodo.id}`)
+      .then(resp => {
+        history('/todos');
+      })
+      .catch(e => {
+        console.log(e);
+      })
+    }
+  }
+
+
+
   return (
-    <div>EditTodo</div>
+    <>
+      <h1>Editing Todo</h1>
+      <div>
+        <div>
+          <label htmlFor="">Current Name</label>
+          <InputName 
+            type="text"
+            name="name"
+            value={currentTodo.name}
+            onChange={handleInputChange}
+          />
+          <div>
+            <span>Current Status</span>
+            <CurrentStatus>
+              {currentTodo.is_completed ? 'Completed' : 'Uncompleted'}
+            </CurrentStatus>
+          </div>
+        </div>
+        {currentTodo.is_completed ? (
+          <IsCompletedButton onClick={() => updateIsCompleted(currentTodo)}>
+            Uncompleted
+          </IsCompletedButton>
+        ) : (
+          <IsCompletedButton onClick={() => updateIsCompleted(currentTodo)}>
+            Completed
+          </IsCompletedButton>
+        )}
+        <EditButton onClick={updateTodo}>
+          Update
+        </EditButton>
+        <DeleteButton onClick={deleteTodo}>
+          Delete
+        </DeleteButton>
+      </div>
+    </>
   )
 }
 
